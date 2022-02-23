87cfc066c58e382a81f3b1f1c39d19ee31b7df37
Omar <omarftt@gmail.com>
2022-02-23 16:56:40 -0500

    second seven

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send(req.header("sec-ch-ua"))
+    res.send(req.header("User-Agent"))
 });
 
 app.listen(3000,() => {
